90499fa9c161a0c51d41cf6e079f062e3970aaf0
Abdelrahman Abdelnabi <abdoumohamed96@gmail.com>
2018-10-19 00:31:23 +0200

    optimize BFS, implement A*, implement DFS

--- puzzle.py
+++ puzzle.py
@@ -1,10 +1,23 @@
 import numpy as numpy
-
+from collections import deque
+from heap import Heap
 
 class State:
 
 	def __init__(self, puzzle_matrix):
 		self.puzzle_matrix = puzzle_matrix
+
+	def __eq__(self, other):
+		return self.equals(other)
+
+	def __hash__(self):
+		result = 1
+		base = 1
+		for i in numpy.nditer(self.puzzle_matrix):
+			result += i * base
+			base *= 10
+
+		return result
 
 	def equals(self, other_state):
 		return numpy.array_equal(self.puzzle_matrix, other_state.puzzle_matrix)
@@ -15,17 +28,13 @@ class Problem:
 		self.initial_state = initial_state
 		self.rows = rows
 		self.cols = cols
-		self.goal_state = State(
-			numpy.array([
-				[1,2,3,4],
-				[5,6,7,8],
-				[9,10,11,12],
-				[13,14,15,0]]))
+		arr = list(range(1,rows * cols))
+		arr.append(0)
+		self.goal_state = State(numpy.array(arr).reshape(rows, cols))
 
 	def find_blank(self, state):
 		idx = numpy.where(state.puzzle_matrix == 0)
 		return idx[0][0], idx[1][0]
-
 
 	# returns the possible actions available from the this state
 	def actions(self, state):
@@ -79,6 +88,13 @@ class Node:
 		self.action = action
 		self.path_cost = path_cost
 
+# def __eq__(self, other):
+#   if isinstance(other, self.__class__):
+#    	return self.state.equals(other.state)
+
+# def __hash__(self):
+# 		return hash(self.state)
+
 	def add_child(self, child_node, action):
 		self.children[action] = child_node
 		child_node.parent = self
@@ -87,18 +103,18 @@ class Node:
 	def children(self):
 		return self.children
 
-class BFS:
+class Solver:
 
 	def __init__(self, problem):
 		self.problem = problem
 
-	def search(self):
+	def BFS(self):
 		node = Node(self.problem.initial_state, None, '', 0)
 
 		if self.problem.goal_test(node.state):
-			return node, "success"
+			return node, "success", 0
 
-		frontier_list = list()
+		frontier_list = deque()
 		frontier_set = set()
 		explored_set = set()
 
@@ -109,7 +125,7 @@ class BFS:
 			if not frontier_list:
 				return node, "failure"
 
-			node = frontier_list.pop(0)
+			node = frontier_list.popleft()
 			frontier_set.remove(node)
 
 			explored_set.add(node.state)
@@ -124,6 +140,101 @@ class BFS:
 					frontier_list.append(child)
 					frontier_set.add(child)
 
+	def DFS(self):
+		node = Node(self.problem.initial_state, None, '', 0)
+
+		if self.problem.goal_test(node.state):
+			return node, "success", 0
+
+		frontier_list = list()
+		frontier_set = set()
+		explored_set = set()
+
+		frontier_list.append(node)
+		frontier_set.add(node.state)
+
+		while True:
+			if not frontier_list:
+				return node, "failure", len(explored_set)
+
+			node = frontier_list.pop()
+			frontier_set.remove(node.state)
+
+			explored_set.add(node.state)
+
+			for action in self.problem.actions(node.state):
+				child = self.child_node(self.problem, node, action)
+
+				if child.state not in explored_set and not child.state in frontier_set:
+					if self.problem.goal_test(child.state):
+						return child, "success", len(explored_set)
+
+					frontier_list.append(child)
+					frontier_set.add(child.state)
+
+	def AStar(self):
+		node = Node(self.problem.initial_state, None, '', 0)
+
+		if self.problem.goal_test(node.state):
+			return node, "success", 0
+
+		frontier_heap = Heap()
+		frontier_set = dict() # key: state, value: cost
+		explored_set = set()
+
+		frontier_heap.add(node, priority=node.path_cost)
+		frontier_set[node.state] = node.path_cost
+
+		while True:
+			if not frontier_heap.pq:
+				return node, "failure", len(explored_set)
+
+			cost, node = frontier_heap.pop()
+			frontier_set.pop(node.state, None)
+
+			if self.problem.goal_test(node.state):
+				return node, "success", len(explored_set)
+
+			explored_set.add(node.state)
+
+			for action in self.problem.actions(node.state):
+				child = self.child_node_manhattan(self.problem, node, action)
+
+				if child.state not in explored_set and child.state not in frontier_set:
+					frontier_heap.add(child, priority=child.path_cost)
+					frontier_set[child.state] = child.path_cost
+				elif child.state in frontier_set:
+					prev_cost = frontier_set[child.state]
+
+					if prev_cost > child.path_cost:
+						frontier_heap.decrease_key(child, child.path_cost)
+
 	def child_node(self, problem, node, action):
 		next_state = problem.next_state(node.state, action)
 		return Node(next_state, node, action, node.path_cost + 1)
+
+	def child_node_manhattan(self, problem, node, action):
+		next_state = problem.next_state(node.state, action)
+		return Node(next_state, node, action, node.path_cost + manhattan_distance(next_state))
+
+def manhattan_distance(state):
+	matrix = state.puzzle_matrix
+	result = 0
+
+	rows = matrix.shape[0]	
+	cols = matrix.shape[1]
+
+	for i in range(rows):
+		for j in range(cols):
+			val = matrix[i][j]
+			if val == 0:
+				expected_col = cols
+				expected_row = rows
+			else:
+				expected_row = (val - 1) // cols
+				expected_col = (val - 1) % cols
+			
+			result += abs(expected_row - i) + abs(expected_col - j)
+	
+	return result
+
--- routes.py
+++ routes.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, send_from_directory, request, jsonify
 import numpy
 import json
 import time
-from puzzle import State, Problem, BFS, Node
+import math
+
+from puzzle import State, Problem, Solver, Node
 
 app = Flask(__name__)      
  
@@ -22,14 +24,26 @@ def send_js(path):
 def solve():
 	print request.get_json()
 	data = request.get_json()
-	print data['sequence']
-	np_array = numpy.array(data['sequence']).reshape(4,4)
+	sequence = data['sequence']
+	strategy = data['strategy']
+	if is_square(len(sequence)):
+		sqrt = int(math.sqrt(len(sequence)))
+		np_array = numpy.array(sequence).reshape(sqrt,sqrt)
 	initial_state = State(np_array)
-	problem = Problem(initial_state, 4, 4)
-	bfs = BFS(problem)
+	problem = Problem(initial_state, sqrt, sqrt)
+	solver = Solver(problem)
 
 	start_time = time.time()
-	node, result, num_explored = bfs.search()
+	
+	if strategy == 'A* Manhattan Distance':
+		node, result, num_explored = solver.AStar()
+	elif strategy == 'A* - Euclidean Distance':
+		node, result, num_explored = solver.BFS()
+	elif strategy == 'BFS':
+		node, result, num_explored = solver.BFS()
+	elif strategy == 'DFS':	
+		node, result, num_explored = solver.DFS()
+
 	running_time = round(time.time() - start_time, 5)
 
 	solution_path_cost = node.path_cost
@@ -46,12 +60,20 @@ def solve():
 		"steps": steps,
 		"nodes explored": num_explored,
 		"running time": running_time,
-		"path cost": solution_path_cost
+		"path cost": len(steps)
 	}
 
 	print solution
 	print jsonify(solution)
 	return jsonify(solution)
+
+
+def is_square(integer):
+    root = math.sqrt(integer)
+    if int(root + 0.5) ** 2 == integer: 
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
   app.run(debug=True)
