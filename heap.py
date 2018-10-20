fbf3053a2e7871eaaf870a5265e1197343aa5256
mohamednazmi1 <mohamed-nazmi@hotmail.com>
2018-10-20 16:14:32 +0200

    update heap deacrease_key() method

--- heap.py
+++ heap.py
@@ -4,41 +4,15 @@ from heapq import heappush, heappop, heapify
 class Heap:
 	def __init__(self):
 		self.pq = []                         # list of entries arranged in a heap
-		# self.entry_finder = {}               # mapping of tasks to entries
-		# self.REMOVED = '<removed-task>'      # placeholder for a removed task
-		# self.counter = itertools.count()     # unique sequence count
-
-	# def add(self, task, priority=0):
-	#     'Add a new task or update the priority of an existing task'
-	#     if task in self.entry_finder:
-	#         remove_task(task)
-	#     count = next(self.counter)
-	#     entry = [priority, count, task]
-	#     self.entry_finder[task] = entry
-	#     heappush(self.pq, entry)
-
-	# def remove(self, task):
-	#     'Mark an existing task as REMOVED.  Raise KeyError if not found.'
-	#     entry = entry_finder.pop(task)
-	#     entry[-1] = self.REMOVED
-
-	# def pop(self):
-	#     'Remove and return the lowest priority task. Raise KeyError if empty.'
-	#     while self.pq:
-	#         priority, count, task = heappop(self.pq)
-	#         if task is not self.REMOVED:
-	#             del self.entry_finder[task]
-	#             return task
-	#     raise KeyError('pop from an empty priority queue')
 
 	def pop(self):
 		return heappop(self.pq)
 
 	def decrease_key(self, node, new_value):
-		for idx in enumerate(pq):
-			if node.state.equals(pq[idx][1].state):
-				pq[idx] = (node.path_cost, node)
-				heapify(pq)
+		for idx, val in enumerate(self.pq):
+			if node.state.equals(self.pq[idx][1].state):
+				self.pq[idx] = (new_value, node)
+				heapify(self.pq)
 				return
 		raise ValueError("element not found")
 
